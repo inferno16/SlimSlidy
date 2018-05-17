e52b505121c353f871de59b73f4ea8e37270e979
Ivan Angelov <inferno16@mail.bg>
2018-05-17 23:42:55 +0300

    Bugfixes tweaks and some improvements. Updated the readme file

--- readme.md
+++ readme.md
@@ -11,13 +11,31 @@ Features
 * Not initializing the slider if there is no tracks or no main track (main track is the one that can be changed by the user if more than one main tracks are defined only the first one is recognized. The other one is automatically assigned to secondary)
 * The slider have all of the important values and styles defined by default
 
+How to use
+----------
+
+There are two ways to create a slider from an element, both of which require the same structure.<br>
+For a correct structure you will need to create a div with one or two child divs. At least one of the child divs should be with attribute main="1".<br><br>
+Couple examples:<br>
+  1. Using one track  
+```HTML
+<div>
+    <div main="1"></div>
+</div>
+```
+  2. Using two tracks
+```HTML
+<div>
+    <div main="1"></div>
+    <div></div>
+</div>
+```
+- First way of making the structures above actual sliders is to add ``class="range_slider"`` on the parent element and call ``SlimSlidy.AutoCreateSliders()`` this will take all elements with ``class="range_slider"`` and will create sliders from them (if the structure is correct)
+- The second way is to use JavaScript to pass elements with the structure shown above into ``SlimSlidy.CreateSliderFromElement(my_element)``
+
+
 Notes
 -----
 
-* In order to use you need to create a div with class "range_slider" with one or two child divs. At least one of the child divs should be with attribute main="1".
-Here's an example:
-``<div class="range_slider">``
-``	<div main="1"></div>``
-``</div>``
 * A CSS body and html reset with height: 100% fixes problem with the dragging feature when there is not enough content on the page to fill the screen. Aside from that it's always a good thing to have the CSS resets.
 * For best results apply "user-select: none;" css rules (all the prefixes) on the range_slider's parent element. This fixes poiter problem that occurs when the slider is dragged fast enough to trigger the browser's drag and drop feature.
--- slimslidy.js
+++ slimslidy.js
@@ -1,145 +1,216 @@
-var elements;
-var sliders = [];
-var md = 0;
-var evt;
+window.SlimSlidy = (function(){
+    'use strict';
+    var elements;
+    var sliders = [];
+    var md = 0;
+    var evt;
+    var lastSlider;
 
-function Track() {
-	this.elem = Object();
-	this.current = 0;
-}
+    function Track() {
+        this.elem = Object();
+        this.current = 0;
+    }
 
-function RangeSlider() {
-	this.self = Object();
-	this.tracks = [];
-	this.min = 0;
-	this.max = 100;
-	this.step = 1;
-	this.setCurrent = function(trackNum, value, percentage) {
-		if(value < this.min) {
-			value = this.min;
-			percentage = 0;
-		}
-		if(value > this.max) {
-			value = this.max;
-			percentage = 100;
-		}
-		this.tracks[trackNum].current = value;
-		updateProgress(this.tracks[trackNum], percentage);
-	};
-}
+    function RangeSlider() {
+        this.self = Object();
+        this.tracks = [];
+        this.min = 0;
+        this.max = 100;
+        this.step = 1;
+    }
 
-window.addEventListener("mousemove", function(e){
-	evt = window.event || e;
-});
+    RangeSlider.prototype.SetCurrent = function(trackNum, percentage, trigger = false) {
+        if(percentage < 0) {
+            percentage = 0;
+        }
+        if(percentage > 1) {
+            percentage = 1;
+        }
+        var sliderRange = this.max - this.min;
+        var offset = percentage * sliderRange;
+        this.tracks[trackNum].current = this.min + offset;
+        UpdateProgress(this.tracks[trackNum], percentage*100);
+        if(!trigger) {
+            return;
+        }
+        var spcevt = new CustomEvent('sliderProgressChanged', {detail: this.tracks[trackNum]});
+        this.self.dispatchEvent(spcevt);
+    }
 
-window.addEventListener("load", function(){
-	elements = document.getElementsByClassName("range_slider");
-	if(elements.length > 0) {
-		constructSliders();
-		if(sliders.length > 0) {
-			addEventListeners();
-		}
-	}
-	document.body.onmouseup = function(){md = 0;};
-	document.body.onmou = function(){md = 0;};
-});
+    window.addEventListener('mousemove', function(e){
+        evt = window.event || e;
+    });
 
-function constructSliders() 
-{
-	for (var i = 0; i < elements.length; i++) {
-		var childCount = elements[i].getElementsByTagName("*").length;
-		if(childCount > 0 && childCount <= 2) {
-			initTracks(elements[i])
-		}
-	}
-}
+    function InitMouseUp() {
+        if(document.body) {
+            document.body.addEventListener('mouseup', MouseUpHandler);
+        }
+        else {
+            document.addEventListener("DOMContentLoaded", function(event) {
+                document.body.addEventListener('mouseup', MouseUpHandler);
+            });
+        }
 
-function initTracks(elem)
-{
-	var tracks = elem.getElementsByTagName("*");
-	var mainTrack = -1;
-	for (var i = 0; i < tracks.length; i++) {
-		if(tracks[i].getAttribute('main') === '1') {
-			mainTrack = i;
-		}
-	}
-	if(mainTrack !== -1) {
-		var slider = new RangeSlider();
-		slider.self = elem;
-		var track1 = new Track();
-		track1.elem = tracks[mainTrack];
-		slider.tracks.push(track1);
-		if (tracks.length === 2) {
-			var track2 = new Track();
-			track2.elem = tracks[1 - mainTrack];
-			slider.tracks.push(track2);
-		}
-		var min = parseInt(elem.getAttribute('min'));
-		var max = parseInt(elem.getAttribute('max'));
-		var step = parseInt(elem.getAttribute('step'));
-		if(Number.isInteger(min))
-			slider.min = min;
-		if(Number.isInteger(max))
-			slider.max = max;
-		if(Number.isInteger(step))
-			slider.step = step;
-		applyStyle(slider);
-		sliders.push(slider);
-	}
-}
+        function MouseUpHandler() {
+            if (md) {
+                ChangeValue(lastSlider, 0);
+                md = 0;
+                var seekendevt = new CustomEvent('sliderSeekEnd', { detail: lastSlider.tracks[0] });
+                lastSlider.self.dispatchEvent(seekendevt);
+            }
+        }
+    }
 
-function applyStyle(slider) {
-	var element = slider.self;
-	element.style.cursor = 'pointer';
-	if(element.style.height === '')
-		element.style.height = "5px";
-	if(element.style.background === '')
-		element.style.background = "#555555";
-	var main = slider.tracks[0].elem;
-	if(main.style.background === '')
-		main.style.background = "#ff0000";
-	if(main.style.height === '')
-		main.style.height = "100%";
-	main.style.float = "left";
-	if(slider.tracks.length > 1) {
-		var second = slider.tracks[1].elem;
-		if(second.style.background === '')
-			second.style.background = "#9d9d9d";
-		if(second.style.height === '')
-			second.style.height = "100%";
-		if(second.style.width === '')
-			second.style.height = "0";
-	}
-}
+    function CreateSliderFromElement(elem) {
+        if(!IsCreated(elem)) {
+            return ConstructSlider(elem);
+        }
+        return GetSliderFromElement(elem);
+    }
 
-function updateProgress(track, percentage) {
-	track.elem.style.width = percentage+"%";
-}
+    function AutoCreateSliders() {
+        elements = document.getElementsByClassName('range_slider');
+        if(elements.length > 0) {
+            ConstructSliders();
+        }
+    }
 
-function changeValue(slider, trackNum) 
-{
-	var mouseX = evt.clientX;
-	var elemRect = slider.self.getBoundingClientRect();
-	var width = elemRect.right - elemRect.left;
-	var relClick = mouseX - elemRect.left;
-	var percentage = relClick / width * 100;
-	var sliderRange = slider.max - slider.min;
-	var offset = percentage * sliderRange / 100;
-	var value = slider.min + offset; //no need for checks setCurrent does it for us
-	slider.setCurrent(trackNum, value, percentage);
-}
+    function IsCreated(elem) {
+        for(var i = 0; i < sliders.length; i++) {
+            if (sliders[i].self === elem) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-function addEventListeners()
-{
-	// using let since the var's scope was causing problems
-	for (let i = 0; i < sliders.length; i++) {
-		sliders[i].self.addEventListener("mousedown", function(){
-			md = 1;
-			changeValue(sliders[i], 0);
-			document.body.onmousemove = function(){
-				if(md)
-					changeValue(sliders[i], 0);
-			};
-		});
-	}
-}
+    function ConstructSliders() 
+    {
+        for (var i = 0; i < elements.length; i++) {
+            if(!IsCreated(elements[i])) {
+                ConstructSlider(elements[i])
+            }
+        }
+    }
+
+    function ConstructSlider(element) {
+        var childCount = element.getElementsByTagName('*').length;
+        if(childCount > 0 && childCount <= 2) {
+            return InitTracks(element)
+        }
+        return null;
+    }
+
+    function InitTracks(elem)
+    {
+        var tracks = elem.getElementsByTagName('*');
+        var mainTrack = -1;
+        for (var i = 0; i < tracks.length; i++) {
+            if(tracks[i].getAttribute('main') === '1') {
+                mainTrack = i;
+            }
+        }
+        if(mainTrack !== -1) {
+            var slider = new RangeSlider();
+            slider.self = elem;
+            var track1 = new Track();
+            track1.elem = tracks[mainTrack];
+            slider.tracks.push(track1);
+            if (tracks.length === 2) {
+                var track2 = new Track();
+                track2.elem = tracks[1 - mainTrack];
+                slider.tracks.push(track2);
+            }
+            var min = parseInt(elem.getAttribute('min'));
+            var max = parseInt(elem.getAttribute('max'));
+            var step = parseInt(elem.getAttribute('step'));
+            var initial = parseInt(elem.getAttribute('initial'));
+            if(Number.isInteger(min))
+                slider.min = min;
+            if(Number.isInteger(max))
+                slider.max = max;
+            if(Number.isInteger(step))
+                slider.step = step;
+            if(Number.isInteger(initial)) {
+                slider.SetCurrent(0, initial/(slider.max-slider.min));
+            }
+            ApplyStyle(slider);
+            sliders.push(slider);
+            AddHandlers(slider);
+            return slider;
+        }
+        return null;
+    }
+
+    function ApplyStyle(slider) {
+        var element = slider.self;
+        element.style.cursor = 'pointer';
+        if(element.style.height === '')
+            element.style.height = '5px';
+        if(element.style.background === '')
+            element.style.background = '#555555';
+        var main = slider.tracks[0].elem;
+        if(main.style.background === '')
+            main.style.background = '#ff0000';
+        if(main.style.height === '')
+            main.style.height = '100%';
+        main.style.float = 'left';
+        if(slider.tracks.length > 1) {
+            var second = slider.tracks[1].elem;
+            if(second.style.background === '')
+                second.style.background = '#9d9d9d';
+            if(second.style.height === '')
+                second.style.height = '100%';
+            if(second.style.width === '')
+                second.style.width = '0';
+        }
+    }
+
+    function UpdateProgress(track, percentage) {
+        track.elem.style.width = percentage+'%';
+    }
+
+    function GetSliderFromElement(elem) {
+        for (let i = 0; i < sliders.length; i++) {
+            if(elem === sliders[i].self) {
+                return sliders[i];
+            }
+        }
+        return null;
+    }
+
+    function ChangeValue(slider, trackNum, trigger = true) 
+    {
+        var mouseX = evt.clientX;
+        var elemRect = slider.self.getBoundingClientRect();
+        var width = elemRect.right - elemRect.left;
+        var relClick = mouseX - elemRect.left;
+        var percentage = relClick / width;
+        slider.SetCurrent(trackNum, percentage, trigger);
+    }
+
+    function AddHandlers(slider)
+    {
+        slider.self.addEventListener('mousedown', function(){
+            md = 1;
+            lastSlider = slider;
+            document.body.onmousemove = function(){
+                setTimeout(function(){
+                    if(md) {
+                        ChangeValue(slider, 0, false);
+                        var seekevt = new CustomEvent('sliderSeek', {detail: lastSlider.tracks[0]});
+                        lastSlider.self.dispatchEvent(seekevt);
+                    }
+                },50);
+            };
+        });
+    }
+
+    InitMouseUp();
+
+    return {
+        GetSliderFromElement: GetSliderFromElement,
+        CreateSliderFromElement: CreateSliderFromElement,
+        AutoCreateSliders: AutoCreateSliders
+    }
+})();
